0e2f5e68fdc82f9d8341619aa440390727b533a3
Auriane REVERDELL <areverdell@gmail.com>
2016-04-15 13:37:53 +0200

    modifying order of tasks

--- simple-todos/imports/ui/App.jsx
+++ simple-todos/imports/ui/App.jsx
@@ -11,67 +11,63 @@ import Task from './Task.jsx';
 
 // App component - represents the whole app
 class App extends Component {
-  handleSubmit(event) {
-    event.preventDefault();
-    // Find the text field via the React ref
-    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+    handleSubmit(event) {
+	event.preventDefault();
+	// Find the text field via the React ref
+	const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
 
-    Tasks.insert({
-      text,
-      createdAt: new Date(), // current time
-    });
- 
-    // Clear form
-    ReactDOM.findDOMNode(this.refs.textInput).value = '';
-  }
- 
-  renderTasks() {
-    return this.props.tasks.map((task) => (
-      <Task key={task._id} task={task} />
-    ));
-  }
+	Tasks.insert({
+	    text,
+	    createdAt: new Date(), // current time
+	});
+
+	// Clear form
+	ReactDOM.findDOMNode(this.refs.textInput).value = '';
+    }
+
+    renderTasks() {
+	return this.props.tasks.map((task) => (
+		    <Task key={task._id} task={task} />
+		    ));
+    }
 
 
-  render() {
-    return (
-      <div className="container">
-        <header>
-          <h1>Todo List</h1>
-          <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+    render() {
+	return (
+		<div className="container">
+		<header>
+		<h1>Todo List</h1>
+		<form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
 
-            <input
+		<input
+		    type="text"
+		    ref="textInput"
+		    placeholder="Type to add new tasks"
+		/>
 
-              type="text"
+		</form>
+		</header>
 
-              ref="textInput"
-
-              placeholder="Type to add new tasks"
-
-            />
-
-          </form>
-        </header>
-
-        <ul>
-          {this.renderTasks()}
-        </ul>
-      </div>
-    );
-  }
+		<ul>
+		    {this.renderTasks()}
+		</ul>
+		</div>
+	    );
+    }
 }
- 
+
 
 App.propTypes = {
-  tasks: PropTypes.array.isRequired,
+    tasks: PropTypes.array.isRequired,
 };
 
 export default createContainer(() => {
-  return {
-	tasks: Tasks.find({}, { sort: { createdAt: 1 } }).fetch(),
-  };
+    return {
+	tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+    };
 }, App);
 
- 
+
 
 
 
